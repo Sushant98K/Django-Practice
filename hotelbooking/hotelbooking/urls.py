c3c5a99c609e259cacd128a487feb0c53e73db67
Sushant98K <sushant98k@gmail.com>
2024-12-28 02:21:45 +0530

    user crud complete

--- hotelbooking/hotelbooking/urls.py
+++ hotelbooking/hotelbooking/urls.py
@@ -24,7 +24,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hotelapp.urls')),
-    
+    path('', include('userapp.urls')),
+     
     
     
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
