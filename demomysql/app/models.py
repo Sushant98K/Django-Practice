b447a1048e44b212113320cbe4ffa534368df473
Sushant98K <sushant98k@gmail.com>
2025-01-01 00:25:51 +0530

    admin pannel edit

--- demomysql/app/models.py
+++ demomysql/app/models.py
@@ -5,3 +5,9 @@ class Demodata(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(max_length=100)
+    
+    
+    
+    #shows object titel in admin panel 
+    def __str__(self):
+        return self.name
