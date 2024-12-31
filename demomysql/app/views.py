14692eed4fe8753b2d1b4c8a44be1e6d3515aebb
Sushant98K <sushant98k@gmail.com>
2025-01-01 00:13:50 +0530

    created super user in mysql server

--- demomysql/app/views.py
+++ demomysql/app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def adddata(request):
     if request.method == 'POST':
         name = request.POST['name']
         age = request.POST['age']
         email = request.POST['email']
-        return render(request, 'app/success.html', {'name': name, 'age': age, 'email': email})
+        
+        new = Demodata(name=name, age=age, email=email)
+        new.save()
+        
+        return render(request, 'app/success.html', {'data': new})
     else:
         return render(request, 'app/index.html')
+
+def showdata(request):
+    data = Demodata.objects.all()
+    return render(request, 'app/showdata.html', {'data': data})
